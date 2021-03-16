1158e4232aed748d254786bae9f4ced4c050de21
Harpreet Somel <somel.harpreet@gmail.com>
2021-03-16 14:13:40 -0700

    working on creating the landing page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import "./App.css";
+import React, { useEffect } from "react";
+import { Switch, Route, Redirect } from "react-router-dom";
+import LandingPage from "./pages/landing-page.component";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code>
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Switch>
+      <Route exact path="/" component={LandingPage} />
+    </Switch>
   );
-}
+};
 
 export default App;
---
+++ src/pages/landing-page.component.js
@@ -0,0 +1,22 @@
+import React from "react";
+// reactstrap components
+import {
+  Button,
+  Input,
+  InputGroupAddon,
+  InputGroupText,
+  InputGroup,
+  Container,
+  Row,
+  Col,
+} from "reactstrap";
+
+function LandingPage() {
+  return (
+    <>
+      <div>landing page</div>
+    </>
+  );
+}
+
+export default LandingPage;
