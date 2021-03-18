e7fce388a3ee8337e4caaf277a46205802c2fd93
Harpreet Somel <somel.harpreet@gmail.com>
2021-03-18 16:02:27 -0700

    created route /dashboard

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from "react";
-import { Switch, Route, Redirect } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 import LandingPage from "./pages/landing-page.component";
 import SignInSignUpPage from "./pages/sign-in-sign-up.component";
+import DashBoardPage from "./pages/dashboard.component";
 
 const App = () => {
   return (
@@ -11,6 +12,11 @@ const App = () => {
         exact
         path="/signin"
         render={(routeProps) => <SignInSignUpPage {...routeProps} />}
+      />
+      <Route
+        exact
+        path="/dashboard"
+        render={(routeProps) => <DashBoardPage {...routeProps} />}
       />
     </Switch>
   );
