558dc80ab65889329c28db99fa96ed0f2b2c6d7e
Harpreet Somel <somel.harpreet@gmail.com>
2021-05-03 15:18:15 -0700

    updated billing chart component

--- src/components/DashBoard/customdashcomponents/BillingChart.component.js
+++ src/components/DashBoard/customdashcomponents/BillingChart.component.js
@@ -1,75 +1,100 @@
 import React from "react";
 // @material-ui/core
 import { makeStyles } from "@material-ui/core/styles";
-import Button from "@material-ui/core/Button";
-// @material-ui/icons
-import AccessTime from "@material-ui/icons/AccessTime";
-import PowerSettingsNewIcon from "@material-ui/icons/PowerSettingsNew";
+import { Bar } from "react-chartjs-2";
 // core components
 import Card from "../dashcomponents/Card/Card.js";
 import CardHeader from "../dashcomponents/Card/CardHeader.js";
 import CardBody from "../dashcomponents/Card/CardBody.js";
-import CardFooter from "../dashcomponents/Card/CardFooter.js";
 import styles from "../../../assets/jss/material-dashboard-react/views/dashboardStyle.js";
 
 const useStyles = makeStyles(styles);
 
-const BillingChart = () => {
-  const classes = useStyles();
-  return (
-    <Card>
-      <CardHeader color="success">
-        <h4 style={{ margin: 0, fontWeight: 700 }}>Billing Chart</h4>
-      </CardHeader>
-      <CardBody>
-        <h4 style={{ marginTop: 10 }}>Notebook Ready to Launch </h4>
-        <p
-          className={classes.cardCategory}
-          style={{
-            marginBottom: 25,
-            marginTop: 10,
-            color: "#303030",
-            fontSize: 15,
-          }}
-        >
-          With your free tier membership of OpenEduML you get 30 minutes access every 24
-          hours to your Jupyter Notebook hosted on OpenEduML's cluster. Where you can
-          build, train, and test Machine-learning models in parallel
-        </p>
-        <p
-          className={classes.cardCategory}
-          style={{
-            marginBottom: 25,
-            marginTop: 10,
-            color: "#303030",
-            fontSize: 15,
-          }}
-        >
-          With your free tier membership of OpenEduML you get 30 minutes access every 24
-          hours to your Jupyter Notebook hosted on OpenEduML's cluster. Where you can
-          build, train, and test Machine-learning models in parallel
-        </p>
+let chartExample3 = {
+  data: (canvas) => {
+    let ctx = canvas.getContext("2d");
 
-        <Button
-          variant="contained"
-          color="secondary"
-          className={classes.button}
-          startIcon={<PowerSettingsNewIcon />}
-          style={{
-            marginBottom: 15,
-            marginTop: 15,
-            minWidth: 140,
-            fontWeight: 700,
-          }}
-        >
-          Launch
-        </Button>
+    let gradientStroke = ctx.createLinearGradient(0, 230, 0, 50);
+
+    gradientStroke.addColorStop(1, "rgba(72,72,176,0.1)");
+    gradientStroke.addColorStop(0.4, "rgba(72,72,176,0.0)");
+    gradientStroke.addColorStop(0, "rgba(119,52,169,0)"); //purple colors
+
+    return {
+      labels: ["USA", "GER", "AUS", "UK", "RO", "BR"],
+      datasets: [
+        {
+          label: "Countries",
+          fill: true,
+          backgroundColor: gradientStroke,
+          hoverBackgroundColor: gradientStroke,
+          borderColor: "#d048b6",
+          borderWidth: 2,
+          borderDash: [],
+          borderDashOffset: 0.0,
+          data: [53, 20, 10, 80, 100, 45],
+        },
+      ],
+    };
+  },
+  options: {
+    maintainAspectRatio: false,
+    legend: {
+      display: false,
+    },
+    tooltips: {
+      backgroundColor: "#f5f5f5",
+      titleFontColor: "#333",
+      bodyFontColor: "#666",
+      bodySpacing: 4,
+      xPadding: 0,
+      mode: "nearest",
+      intersect: 0,
+      position: "nearest",
+    },
+    responsive: true,
+    scales: {
+      yAxes: [
+        {
+          gridLines: {
+            drawBorder: false,
+            color: "rgba(225,78,202,0.1)",
+            zeroLineColor: "transparent",
+          },
+          ticks: {
+            suggestedMin: 60,
+            suggestedMax: 120,
+            padding: 0,
+            fontColor: "#9e9e9e",
+          },
+        },
+      ],
+      xAxes: [
+        {
+          gridLines: {
+            drawBorder: false,
+            color: "rgba(225,78,202,0.1)",
+            zeroLineColor: "transparent",
+          },
+          ticks: {
+            padding: 0,
+            fontColor: "#9e9e9e",
+          },
+        },
+      ],
+    },
+  },
+};
+
+const BillingChart = () => {
+  return (
+    <Card style={{ minHeight: 340 }}>
+      <CardHeader color="warning">
+        <h4 style={{ margin: 0, fontWeight: 700 }}>Monthly Billing History</h4>
+      </CardHeader>
+      <CardBody style={{ marginTop: 10 }}>
+        <Bar data={chartExample3.data} options={chartExample3.options} />
       </CardBody>
-      <CardFooter chart>
-        <div className={classes.stats}>
-          <AccessTime /> last launched: 4 minutes ago
-        </div>
-      </CardFooter>
     </Card>
   );
 };
